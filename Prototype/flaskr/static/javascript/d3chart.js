b1b29d60b1b6d12d531ef4dec2a4f5ba004de8ed
Saikiran Solanki <saikiransolankid@gmail.com>
2020-05-31 03:01:35 +1000

    edited chart code; added display of info;

--- Prototype/flaskr/static/javascript/d3chart.js
+++ Prototype/flaskr/static/javascript/d3chart.js
@@ -1,80 +1,176 @@
 function init(){
 
-    var w=300;
-    var h=300;
+    var w=400;
+    var h=350;
   
-    var dataset1=[12,24,18,20];
+    var dataset1={ "Carbon":24, "Methane": 28, "Nitrousoxide": 15 };
   
-    var outerRadius = 150;
+    var outerRadius = 100;
     var innerRadius = 0;
   
     var arc= d3.arc()
                 .outerRadius(outerRadius)
-                .innerRadius(innerRadius+80);
+                .innerRadius(innerRadius+40);
+    var arcOver=d3.arc()
+                    .outerRadius(outerRadius+10)
+                    .innerRadius(innerRadius+50);
+  
   
     var pie = d3.pie();
-  
+    
+    var dataset=d3.values(dataset1);
+
     var svg = d3.select("#d3charts")
                 .append("svg")
                 .attr("width",w)
                 .attr("height",h);
   
     var arcs = svg.selectAll("g.arc")
-                  .data(pie(dataset1))
+                  .data(pie(dataset))
                   .enter()
                   .append("g")
                   .attr("class","arc")
-                  .attr("transform","translate("+ outerRadius +","+ outerRadius +")");
+                  .attr("transform","translate("+ 120 +","+ 150 +")");
+    
+   
+    var color = d3.scaleOrdinal()
+                    .domain(dataset1)
+                    .range(['#96C5F7','#6DC0D5','#C2EFEB']);
+  
+    svg.append("rect")
+        .attr("x", 275)
+        .attr("y", 100)
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill","#96C5F7")
+        .attr("id","co2_square");
+    
+    svg.append("text")
+        .attr("x",300)
+        .attr("y",112)
+        .text("CO2 content")
+        .attr("id","co2_text");
+    
+    svg.append("rect")
+        .attr("x", 275)
+        .attr("y", 130)
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill","#6DC0D5")
+        .attr("id","ch4_square");
 
-    var arcOver=d3.arc()
-                .outerRadius(outerRadius+50)
-                .innerRadius(innerRadius+130);
-  
-    var color = d3.scaleOrdinal(['#fef0d9','#fdcc8a','#fc8d59','#e34a33','#b30000']);
-  
+    svg.append("text")
+        .attr("x",300)
+        .attr("y",142)
+        .text("CH4 content")
+        .attr("id","ch4_text");
+    
+    svg.append("rect")
+        .attr("x", 275)
+        .attr("y", 160)
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill","#C2EFEB")
+        .attr("id","n2o_square");
+    
+    svg.append("text")
+        .attr("x",300)
+        .attr("y",172)
+        .text("N2O Content")
+        .attr("id","n2o_text");
+
+    svg.append("text")
+        .attr("x",20)
+        .attr("y",320)
+        .attr("id","result_text")
+        .style("visibility","hidden");
+
     arcs.append("path")
         .attr("fill", function(d,i){
             return color(i);
         })
         .attr("d", function(d,i){
             return arc(d,i);
+        })
+        .on("mouseover",function(d){
+           
+            d3.select(this)
+            .attr("stroke","white")
+            .transition()
+            .duration(200)
+            .attr("d", arcOver)
+            .attr("stroke-width",1);
+            
+            d3.select("#result_text")
+                .style("visibility","visible")
+                .text(d.value+" -e tonnes")
+                .attr("font-weight","600")
+                .attr("font-size","36");
+            
+           
+            
+            for(var i=0;i<3;i++)
+            {   
+                if(dataset1["Carbon"]==d.value){     
+                    d3.select("#co2_square")
+                        .attr("stroke","rgb(0,0,0)")
+                        .attr("stroke-width","1");
+                    
+                    d3.select("#co2_text")
+                        .attr("font-weight","600");                
+                }
+                else if(dataset1["Methane"]==d.value){ 
+                    d3.select("#ch4_square")
+                        .attr("stroke","rgb(0,0,0)")
+                        .attr("stroke-width","1");
+
+                    d3.select("#ch4_text")
+                        .attr("font-weight","600");                
+                }
+                else if(dataset1["Nitrousoxide"]==d.value){ 
+                    d3.select("#n2o_square")
+                        .attr("stroke","rgb(0,0,0)")
+                        .attr("stroke-width","1");                       
+                    d3.select("#n2o_text")
+                        .attr("font-weight","600");                
+                }
+            }   
+
+
+        })
+        .on("mouseleave", function(d){
+            d3.select(this)
+            .transition()
+            .duration(200)
+            .attr("d", arc)
+            .attr("stroke","none");
+
+            d3.select("#result_text")
+                .style("visibility","hidden");
+
+                if(dataset1["Carbon"]==d.value){     
+                    d3.select("#co2_square")
+                        .attr("stroke-width","0");
+                    
+                    d3.select("#co2_text")
+                        .attr("font-weight","100");                
+                }
+                else if(dataset1["Methane"]==d.value){ 
+                    d3.select("#ch4_square")
+                        .attr("stroke-width","0");
+
+                    d3.select("#ch4_text")
+                        .attr("font-weight","100");                
+                }
+                else if(dataset1["Nitrousoxide"]==d.value){ 
+                    d3.select("#n2o_square")
+                        .attr("stroke-width","0");                       
+                    d3.select("#n2o_text")
+                        .attr("font-weight","100");                
+                }
         });
+  
     
-    arcs.on("mouseover",function(d){
-        
-        d3.select(this)
-        .attr("stroke","white")
-        .transition()
-        .duration(200)
-        .attr("d", function(d,i){
-            return arcOver(d,i);
-        })
-        .attr("stroke-width",10);
-        
-        /*svg.append("text")
-        .text(function(d){
-            return this.value;
-        })
-        .attr("transform", function(d){
-            return "translate("+ arc.centroid(d)+")"
-        });*/
-    })
-    .on("mouseleave", function(d){
-        d3.select(this)
-        .transition()
-        .duration(200)
-        .attr("d", arc)
-        .attr("stroke","none");
-    });
-  
-    /*arcs.append("text")
-        .text(function(d){
-            return d.value;
-        })
-        .attr("transform", function(d){
-            return "translate("+ arc.centroid(d)+")"
-        });*/
-  
   
   
   }
