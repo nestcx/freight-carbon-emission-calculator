47851a508041e58fe27baee398c6f5f4525c14ef
Huseyin Burak Erozkan <101546770@student.swin.edu.au>
2020-08-04 18:12:17 +1000

    Minor changes made
    These changes were pushed to ensure everyone is working on the same version.
    
    Note that a file called mapapiconnector.py was not pushed yet, so the /route route won't work. I will push that file soon

--- Prototype/flaskr/__init__.py
+++ Prototype/flaskr/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from . import mapapiconnector as mapapi # need a . when importing from the same level, for some strange reason (its called relative imports if wanting to google it)
 
 def create_app():
   app = Flask(__name__, instance_relative_config=True)
@@ -7,6 +7,25 @@ def create_app():
   @app.route("/")
   def main():
     return render_template("main.html")
+  
+  @app.route("/dashboard")
+  def dashboard():
+    return render_template("dashboard.html")
+
+  # TODO: THIS ROUTE WON'T WORK AS THE MAPAPICONNECTOR FILE HASN'T BEEN PUSHED TO THE REPO
+  # Find the route between 2 specified coordinates or addresses
+  # Returns the data in GeoJSON format
+  @app.route("/route")
+  def getRoute():
+    return mapapiconnector.connectToApi()
+
+  # This will be used for asynchronous requests.
+  # Find suggestions for address/places depending on the user's input so far
+  # Returns a list of possible addresses if found
+  # Note that this function should throttle requests to minimise loads 
+  @app.route("/autocomplete/address")
+  def findCoordinateOfAddress():
+    # Add functionality here
   
   return app
 
---
+++ Prototype/flaskr/static/javascript/autocomplete.js
@@ -0,0 +1,9 @@
+// This script is used to send asynchronous requests to the web server to autocomplete the address the user is entering
+
+startLocInput = document.getElementById("input-pick-up");
+endLocInput = document.getElementById("input-drop-off");
+
+startLocInput.addEventListener("keypress", function() {
+
+});
+
