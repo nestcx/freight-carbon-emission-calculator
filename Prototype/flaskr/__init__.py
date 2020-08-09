600875bd4212e2754e8c23a67c94057ec299d3b3
Huseyin Burak Erozkan <101546770@student.swin.edu.au>
2020-08-09 18:34:02 +1000

    Added some extra functionality for pulling data from API
    Added some extra routes and functionality to pull data from the API, including things like getting the distance and estimated time for trip.
    
    Also added docstrings to make python code more readable.
    
    Address autosuggest functionality not yet included as its still a work in progress

--- Prototype/flaskr/__init__.py
+++ Prototype/flaskr/__init__.py
@@ -21,20 +21,34 @@ def create_app():
     if "startCoords" in request.args and "endCoords" in request.args:
       startCoords = request.args.get("startCoords")
       endCoords = request.args.get("endCoords")
-      return maproutes.getRoute(startCoords, endCoords)
 
+      # Return the GeoJSON data and a status code of 200 indicating success
+      return maproutes.getRoute(startCoords, endCoords), 200
     else:
+      # TODO: Handle error more gracefully
       return "Need to input coordinates as request arguments"
 
 
   # This will be used for asynchronous requests.
   # Find suggestions for address/places depending on the user's input so far
   # Returns a list of possible addresses if found
-  # Note that this function should throttle requests to minimise loads 
-  @app.route("/autocomplete/address")
-  def findCoordinateOfAddress():
-    # Add functionality here
-    print("Todo")
+  # Note that this function should throttle requests to about 5 per seconds to minimise loads and prevent abuse 
+  @app.route("/search/address", methods=["GET"])
+
+  ######################################################################
+  # TODO: THROTTLE REQUESTS TO ENSURE NO MORE THAN 5 REQUESTS PER SECOND
+  ######################################################################
   
+  def findCoordinateOfAddress():
+    if "input" in request.args:
+      userInput = request.args.get("input")
+      # Return the JSON data and a status code of 200 indicating success
+      return maproutes.searchAddress(userInput), 200
+    else:
+      # TODO: Handle error more gracefully
+      return "Need to input something"
+  
+
+
   return app
 
