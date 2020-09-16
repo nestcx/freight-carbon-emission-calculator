df5b1bb33bfda7a177ad27e93b6d81557fc5ef72
nestcx <marcusvalentino2013@gmail.com>
2020-09-16 17:09:24 +1000

    implement routes GET /shipments and GET /shipments/{id} with authentication

--- afcc_web_app/afcc/shipment/views.py
+++ afcc_web_app/afcc/shipment/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, flash, redirect, url_for
 from afcc.extensions import db
 from afcc.shipment.models import Shipment
+from afcc.user.models import User
+from flask_login import current_user
 
 # create shipment blueprint.
 shipment_bp = Blueprint('shipments', __name__)
@@ -9,13 +11,33 @@ shipment_bp = Blueprint('shipments', __name__)
 # GET  /shipments  -  get a list of shipments
 # POST /shipments  -  create multiple shipments
 @shipment_bp.route('/shipments', methods=['GET', 'POST'])
-def C_shipment():
-    return 'not implemented'
+def CR_shipments():
+
+    # check that user is logged in.
+    if not current_user.is_authenticated:
+        return str("hello!")
+
+    # try to find user's details.
+    try:
+        user = User.query.filter_by(email=current_user.email).first()
+    except Exception:
+        flash("An error has occurred.")
+        return redirect(url_for('display_error_page'))
+
+    # find all shipments that belong to the user.
+    if (request.method == 'GET'):
+        shipments = Shipment.query.filter_by(uid=user.uid).all()
     
+    shipment_names = []
+    for shipment in shipments:
+        shipment_names.append(shipment.shipment_name)
+    
+    return str(shipment_names)
+
 
 # POST  /shipment  -  create a new shipment
 @shipment_bp.route('/shipment', methods=['POST'])
-def create_shipment():
+def C_shipment():
     return 'not implemented'
 
 
@@ -30,7 +52,38 @@ def show_create_shipment_form():
 # DELETE  /shipments/<shipment_id>  -  delete individual shipment
 @shipment_bp.route('/shipments/<int:shipment_id>', methods=['GET', 'PATCH', 'DELETE'])
 def RUD_shipment(shipment_id):
-    return 'not implemented'
+
+    # check that user is logged in.
+    if not current_user.is_authenticated:
+        return redirect(url_for('user.log_in'))
+
+    # try to find user's details.
+    try:
+        user = User.query.filter_by(email=current_user.email).first()
+    except Exception:
+        flash("An error has occurred.")
+        return redirect(url_for('display_error_page'))
+    
+    # GET 
+    if (request.method == 'GET'):
+
+        # query shipment.
+        shipment = Shipment.query.get(shipment_id)
+
+        # if shipment does not exist, or does not belong to user, redirect to shipments list.
+        # if shipment belongs to user, display shipment.
+        if (shipment is None or user.uid != shipment.uid):
+            return redirect(url_for('shipments.CR_shipments')) 
+        elif (user.uid == shipment.uid):
+            return str("shipment")
+
+    # PATCH
+    if (request.method == 'PATCH'):
+        None
+
+    # DELETE
+    if (request.method == 'DELETE'):
+        None
 
 
 # GET  /shipments/<shipment_id>/edit  -  show form to edit individual shipment
