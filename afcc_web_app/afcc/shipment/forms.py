089093afa5c3d29c0a9d34cb3bacea7e15f6c13f
James <james.howell1708@gmail.com>
2020-09-22 19:26:01 +1000

    Fixed Form File
    Removed requirement for unit and id from update form

--- afcc_web_app/afcc/shipment/forms.py
+++ afcc_web_app/afcc/shipment/forms.py
@@ -4,9 +4,6 @@ from wtforms.validators import DataRequired, EqualTo, Length
 
 # Update Shipment Form
 class UpdateForm(FlaskForm):
-    id = IntegerField('id', validators=[DataRequired(
-        message='Shipment Id must be present')])
-
     toAddress = StringField('to', validators=[
         DataRequired(message="Please enter end destination address")])
 
