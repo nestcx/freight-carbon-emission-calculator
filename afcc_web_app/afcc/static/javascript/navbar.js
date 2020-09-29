4e9e24815072ff72a85e0e95cdb129c079773017
Huseyin Burak Erozkan <101546770@student.swin.edu.au>
2020-09-29 16:31:01 +1000

    Major changes and clean up to the frontend code

--- afcc_web_app/afcc/static/javascript/navbar.js
+++ afcc_web_app/afcc/static/javascript/navbar.js
@@ -2,11 +2,11 @@
 
 function hamburgerMenu() {
 
-  console.log("test");  
   var navbar = document.getElementsByClassName("navbar")[0];
   var hamburgerButtonLines = document.getElementsByClassName("button-hamburger--line");
 
   navbar.classList.toggle("--hidden-on-mobile");
+  document.addEventListener('click', clickedOutOfNavbar);
 
   for (var i = 0; i < hamburgerButtonLines.length; i++) {
     hamburgerButtonLines[i].classList.toggle("--toggled-on");
@@ -15,15 +15,52 @@ function hamburgerMenu() {
 
 function dropdownMenu() {
   var accountDropdown = document.getElementById("accountDropdown");
+  accountDropdown.classList.toggle("--show-content");
+}
 
-  accountDropdown.classList.toggle("show")
+/**
+ * Check to see if a user has clicked outside of the navbar. This function
+ * will only be called by the eventlistener that is created when the hamburger
+ * button is clicked.
+ * @param {event} e The event that was fired 
+ */
+function clickedOutOfNavbar(e) {
+  var navbar = document.getElementsByClassName("navbar")[0];
+  var hamburgerButton = document.getElementsByClassName("button-hamburger")[0];
+
+  // If the user clicked outside of the hamburger button or navbar menu,
+  // they probably want it to close.
+  if (!(navbar.contains(e.target) || hamburgerButton.contains(e.target))) {
+    //console.log('the clicked target is OUTSIDE navbar or hamburger button');
+
+    // The navbar will always be open if this code is reached, since the eventlistener
+    // only exists when the navbar is open. Therefore, toggle is effectively the same
+    // as remove class name
+    navbar.classList.toggle("--hidden-on-mobile");
+    
+    // Delete the event listener that fired this function.
+    document.removeEventListener('click', clickedOutOfNavbar);
+    
+    var hamburgerButtonLines = document.getElementsByClassName("button-hamburger--line");
+    for (var i = 0; i < hamburgerButtonLines.length; i++) {
+      hamburgerButtonLines[i].classList.toggle("--toggled-on");
+    }
+  }
+
 }
 
 window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var accountDropdown = document.getElementById("accountDropdown");
-    if (accountDropdown.classList.contains('show')) {
-      accountDropdown.classList.remove('show');
+  // Get the parent element, so that we can check if the user clicked anywhere within
+  // the drop down menu or user icons
+  var accountDropdownParent = document.getElementById("user-account-links-container");
+
+  // If user clicked outside of the dropdown menu buttons
+  if (!accountDropdownParent.contains(e.target)) {
+    var accountDropdown = document.getElementById("accountDropdown");
+    if (accountDropdown.classList.contains('--show-content')) {
+      accountDropdown.classList.remove('--show-content');
     }
   }
+
+
 }
