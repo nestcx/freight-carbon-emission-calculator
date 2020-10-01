a7c0765064554404c6453487203ab7153557deb1
nestcx <marcusvalentino2013@gmail.com>
2020-10-01 15:52:47 +1000

    update and clean-up shipment endpoints

--- afcc_web_app/afcc/shipment/forms.py
+++ afcc_web_app/afcc/shipment/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, SubmitField
+from wtforms import StringField, DecimalField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class CreateShipmentForm(FlaskForm):
@@ -21,4 +21,35 @@ class CreateShipmentForm(FlaskForm):
         DataRequired(message='Please enter a load weight')
     ])
 
+    load_weight_unit = SelectField('Load Weight Unit', choices=[
+        ('kilogram', 'Kilograms'), 
+        ('tonne', 'Tonnes')],
+        validators = [DataRequired(message='Please enter a load weight unit')])
+
     submit = SubmitField('Create Shipment')
+
+class EditShipmentForm(FlaskForm):
+    shipment_name = StringField('Shipment Name', validators =[
+        Length(min=4, message='Shipment name must be at least 4 characters')
+    ])
+
+    start_address = StringField('Start Address', validators =[
+        DataRequired(message='Please enter a start location'),
+        Length(min=2, message='Start location must be longer than 2 characters')
+    ])
+
+    end_address = StringField('End Address', validators =[
+        DataRequired(message='Please enter an end location'),
+        Length(min=2, message='End location must be longer than 2 characters')
+    ])
+
+    load_weight = DecimalField('Load Weight', validators=[
+        DataRequired(message='Please enter a load weight')
+    ])
+
+    load_weight_unit = SelectField('Load Weight Unit', choices=[
+        ('kilogram', 'Kilograms'), 
+        ('tonne', 'Tonnes')], 
+        validators = [DataRequired(message='Please enter a load weight unit')])
+
+    submit = SubmitField('Edit Shipment')
