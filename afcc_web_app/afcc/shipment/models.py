07d1c4c9b6dfb9d9f8e772211499d153b3c530c9
nestcx <marcusvalentino2013@gmail.com>
2020-10-05 22:53:52 +1100

    create truck configuration model

--- afcc_web_app/afcc/shipment/models.py
+++ afcc_web_app/afcc/shipment/models.py
@@ -21,3 +21,15 @@ class Shipment(db.Model):
     start_address_coordinates = db.Column(db.String(255), nullable=False)
     end_address = db.Column(db.String(255), nullable=False)
     end_address_coordinates = db.Column(db.String(255), nullable=False)
+
+class TruckConfiguration(db.Model):
+
+    __tablename__ = 'shipment'
+
+    configuration_id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(50), nullable=False)
+    gcm = db.Column(db.Float, nullable=False)
+    payload = db.Column(db.Float, nullable=False)
+    fuel_economy = db.Column(db.Integer, nullable=False)
+    overall_length = db.Column(db.Float, nullable=False)
