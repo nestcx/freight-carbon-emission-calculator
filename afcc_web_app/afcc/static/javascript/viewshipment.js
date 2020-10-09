9ce264772ef5cfa5376a7db2a41f915254aba44c
James <james.howell1708@gmail.com>
2020-10-09 21:52:28 +1100

    Added table selection options and added styling

--- afcc_web_app/afcc/static/javascript/viewshipment.js
+++ afcc_web_app/afcc/static/javascript/viewshipment.js
@@ -8,6 +8,40 @@ function expandinfo() {
     }
 }
 
+function calculateTotal() {
+
+}
+
 function calculateCost() {
-    
+
+}
+
+function calculateTrees() {
+
+}
+
+function modify() {
+    document.getElementsByClassName("btn-edit")[0].classList.toggle("--hidden");
+    document.getElementsByClassName("btn-delete")[0].classList.toggle("--hidden");
+}
+
+function rowClickHandlers(tableId) {
+    var table = document.getElementById(tableId);
+    var rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        var currentRow = table.rows[i];
+        var createClickHandler = function(row) {
+            return function() {
+                var cell = row.getElementsByTagName("td")[0];
+                var id = cell.innerHTML;
+                alert(id);
+            };
+        };
+        currentRow.onclick = createClickHandler(currentRow);
+    }
+}
+
+document.onload = loadFunction() {
+    rowClickHandlers("table--emissions");
+    rowClickHandlers("tablers--emissions");
 }
