04878f0d4ae9da58e9ce5b8208a844f619cc6b27
James <james.howell1708@gmail.com>
2020-10-11 17:22:47 +1100

    Added Calculations
    Added emisisons total calcualtion and price calculation

--- afcc_web_app/afcc/static/javascript/viewshipment.js
+++ afcc_web_app/afcc/static/javascript/viewshipment.js
@@ -11,7 +11,11 @@ function init(){
     
     
     draw_coins(x,y);
-    rowClickHandler("table--pricing");
+    costClickHandler("table--pricing");
+    emissionClickHandler("table--emissions");
+    calculateTotal();
+    calculateCost("AU");
+
 }
 
 function expandinfo() {
@@ -22,11 +26,58 @@ function expandinfo() {
     }
 }
 
+//calculates total emission from emission table data and fills html element with value
 function calculateTotal() {
-
+    //getting table and row elements from DOM
+    var table = document.getElementById("table--emissions");
+    var rows = table.getElementsByTagName("tr");
+    var i;
+    var total = 0;
+    //iterates through emission table and adds data value to running total
+    for (i = 0; i < rows.length; i++) {
+        var currentRow = table.rows[i];
+        var emission = parseFloat(currentRow.getElementsByTagName("td")[1].innerHTML);
+        total = total + emission;
+    }
+    //sets total emission element to total value
+    document.getElementById("emissionsTotal").innerHTML = total + " tCO2e";
 }
 
-function calculateCost() {
+function calculateCost(currency) {
+    var price;
+    var text;
+    var unit;
+    switch (currency) {
+        case "AU":
+            price = 24.15;
+            unit = "&#36 "  //ASCII code for dollar symbol
+            text = "This is how much your shipment would cost based on the repealed 2014 Australian carbon tax";
+        break;
+        case "CAL":
+            price = 15;
+            unit = "&#36 "       //ASCII code for dollar symbol
+            text = "This is how much your shipment would cost under the Californian carbon trading scheme"
+        break;
+        case "GB":
+            price = 25;
+            unit = "&#163 " //ASCII code for pound symbol
+            text = "This is how much your shipment would cost under Great Britains current Cap and Trade policy"
+        break;
+        case "UN":
+            price = 135;
+            unit = "&#36 "  //ASCII code for dollar symbol
+            text = "This is how much your shipment would cost under the United Nations recommended pricing to keep global temperature increase to below 1.5 degrees"
+        break;
+        default:
+            price = 24.15;
+            unit = "&#36 "  //ASCII code for dollar symbol
+            text = "This is how much your shipment would cost based on the repealed 2014 Australian carbon tax";
+    }
+    //calculates 
+    var emissions = document.getElementById("emissionsTotal").innerHTML;
+    var cost = price * emissions;
+    document.getElementById("calculatedPrice").innerHTML = unit + cost;
+    document.getElementById("priceDescription").innerHTML = text;
 
 }
 
@@ -34,30 +85,58 @@ function calculateTrees() {
 
 }
 
+//opens and closes edit and delete button options
 function modify() {
     document.getElementsByClassName("btn-edit")[0].classList.toggle("--hidden");
     document.getElementsByClassName("btn-delete")[0].classList.toggle("--hidden");
 }
 
-function priceClickHandler(tableId) {
+
+//click handlers for table rows, creates onClick event based on table row data
+function costClickHandler(tableId) {
     var table = document.getElementById(tableId);
     var rows = table.getElementsByTagName("tr");
     var i;
     for (i = 0; i < rows.length; i++) {
+        var id = rows[i].id;
         var currentRow = table.rows[i];
-        var createClickHandler = function(row) {
+        var createClickHandler = function(id, tableId) {
             return function() {
-                var cell = row.getElementsByTagName("td")[0];
-                var id = cell.innerHTML;
-                alert(id);
+                makeSelected(id, tableId);
+                calculateCost(id);
             };
         };
-        currentRow.onclick = createClickHandler(currentRow);
+        currentRow.onclick = createClickHandler(id, tableId);
     }
 }
 
-function rowClick() {
-    alert("test");
+function emissionClickHandler(tableId) {
+    var table = document.getElementById(tableId);
+    var rows = table.getElementsByTagName("tr");
+    var i;
+    for (i = 0; i < rows.length; i++) {
+        var id = rows[i].id;
+        var currentRow = table.rows[i];
+        var createClickHandler = function(id, tableId) {
+            return function() {
+                makeSelected(id, tableId);
+            };
+        };
+        currentRow.onclick = createClickHandler(id, tableId);
+    }
+}
+
+//adds "--selected" to class list of given row within given table
+function makeSelected(rowId, tableId) {
+    //Deselect all rows
+    var table = document.getElementById(tableId);
+    var elements = table.getElementsByTagName("tr");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("--selected");
+    }
+    //Make clicked row selected
+    document.getElementById(rowId).classList.add("--selected");
 }
 
 window.onload = init;
