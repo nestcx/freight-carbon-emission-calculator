5e5ee6760069c621132001adda2a609d25a85a2a
nestcx <marcusvalentino2013@gmail.com>
2020-10-21 19:30:46 +1100

    create and add tools blueprint

---
+++ afcc_web_app/afcc/tools/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, render_template, url_for
+from afcc.extensions import db
+from afcc.shipment.models import Shipment
+from afcc.user.models import User
+from flask_login import current_user
+
+# create tools blueprint.
+tools_bp = Blueprint(
+    'tools', __name__, template_folder='tools-templates', static_folder='tools-static')
+
+@tools_bp.route('/tools', methods=['GET'])
+def tools():
+    return render_template('tools.html')
